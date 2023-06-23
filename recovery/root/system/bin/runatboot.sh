51b4d1e3d509e02a338171ba694d08d911ddf370
MD Raza <raza231198@gmail.com>
2023-06-23 10:14:29 +0000

    guamp: switch twrp way to load modules
    Signed-off-by: MD Raza <raza231198@gmail.com>
    Change-Id: I8e84e665d8f652709c0b8c5a6d5e89ddb2016367

--- recovery/root/system/bin/runatboot.sh
+++ recovery/root/system/bin/runatboot.sh
@@ -1,29 +1,9 @@
 #!/system/bin/sh
 
-module_path=/vendor/lib/modules
-
 touch_class_path=/sys/class/touchscreen
 touch_path=
 firmware_path=/vendor/firmware
 firmware_file=
-
-# Load TouchScreen Modules
-insmod $module_path/himax_v3_mmi_hx83102d.ko
-insmod $module_path/himax_v3_mmi.ko
-insmod $module_path/ili9882_mmi.ko
-insmod $module_path/chipone_tddi_mmi.ko
-insmod $module_path/nova_0flash_mmi.ko
-
-# Load Needed Modules
-insmod $module_path/sensors_class.ko
-insmod $module_path/utags.ko
-insmod $module_path/exfat.ko
-insmod $module_path/mmi_annotate.ko
-insmod $module_path/mmi_info.ko
-insmod $module_path/mmi_sys_temp.ko
-insmod $module_path/moto_f_usbnet.ko
-insmod $module_path/qpnp-power-on-mmi.ko
-insmod $module_path/qpnp_adaptive_charge.ko
 
 # Fix up chipone
 if [[ -d /sys/class/touchscreen/ICNL9911S ]]; then
